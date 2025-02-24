392378d36dc920e1748cb0991d118f7f7a4879a2
Shivangkrishn26 <113164098+Shivangkrishn26@users.noreply.github.com>
2025-02-24 23:56:52 +0530

    Designing completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,33 @@
+import { Search } from "lucide-react";
+import RecipeCard from "../components/RecipeCard";
 const HomePage = () => {
-    return (
-      <div className="bg-[#faf9fb p-10 flex-1 border-8 border-indigo-600">
-      HomePage
-      <div className="Flex border-4"> regakfgnzkjssngzlkgnmlkzndg</div>
+  return (
+    <div className="bg-[#faf9fb p-10 flex-1 border-8 border-indigo-600">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2">
+            <Search size={24} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="What do u wanna cook today ?"
+            />
+          </label>
+        </form>
+        <h1 className=" font-bold text-3cl md:text-5xl mt-4"> Recommended</h1>
+        <p className=" text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular Choices
+        </p>
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* 1st recipe */}
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
       </div>
-    )
-  }
+    </div>
+  );
+};
 
-  export default HomePage
+export default HomePage;
